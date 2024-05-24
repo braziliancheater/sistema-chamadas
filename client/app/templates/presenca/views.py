ada4ea191e84671c93270fe6626ac0e037ae44be
brazilian <gabrielelerlima80@gmail.com>
2024-05-24 20:05:38 -0300

    reup

--- client/app/templates/presenca/views.py
+++ client/app/templates/presenca/views.py
@@ -5,4 +5,5 @@ from . import presencas
 @presencas.route('/presencas_iniciar', methods=['POST'])
 def presencas_iniciar():
     if request.method == 'POST':
-        
+        ra = request.form['ra']
+        print(ra)
